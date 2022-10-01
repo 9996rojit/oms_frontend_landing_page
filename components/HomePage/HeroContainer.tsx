9dfd4b4bf83b84caf30b34265153d9519990ea8f
Ayush <ayushbaral17@gmail.com>
2022-10-01 23:36:00 +0545

    feat:add below container in hero section

--- components/HomePage/HeroContainer.tsx
+++ components/HomePage/HeroContainer.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Seemlessly from '../../public/assets/seemlessly1.svg';
 import MaintainYour from '../../public/assets/Maintain Your.svg';
 import SupplyChain from '../../public/assets/Supply Chain.svg';
+import arrow from '../../public/assets/arrow.svg';
 
 const HeroContainer = () => {
   return (
@@ -11,7 +12,7 @@ const HeroContainer = () => {
         <Image src={Seemlessly} layout='fill' alt='Seemlessly' />
       </div> */}
 
-      <p className='stoke text-8xl' style={{ fontFamily: 'okine' }}>
+      <p className=' text-white text-8xl' style={{ fontFamily: 'okine' }}>
         Seemlessly
       </p>
 
@@ -20,6 +21,20 @@ const HeroContainer = () => {
       </div>
       <div className='relative w-[646px] h-[134px]'>
         <Image src={SupplyChain} layout='fill' alt='Supply Chain' />
+      </div>
+
+      <div className='flex justify-between mt-[100px]'>
+        <div className='relative h-[85px] w-[85px]'>
+          <Image src={arrow} layout='fill' alt='arrow' />
+        </div>
+
+        <p
+          className='w-[325px] text-white'
+          style={{ fontFamily: 'okineNormal' }}
+        >
+          We Provide You The Best interface To Keep Your Business Intact With
+          Each Other By Maintaining Your Inventory
+        </p>
       </div>
     </div>
   );
